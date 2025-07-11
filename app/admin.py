eedfcbe0e346a1e747e58a53a893cfede175ffad
jonathan-ndayisenga <brucendayisenga22@gmail.com>
2025-07-11 12:50:31 +0300

    Set up initial models and migrations for AutoCare app

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from .models import (
+    Profile, Customer, Vehicle, ServiceTicket,
+    ServiceCategory, DefinedService, ServiceOffered, Part, PartUsed, Page
+)
+
+# Use the actual custom user model
+User = get_user_model()
+admin.site.register(User)
+
+# Register all the rest
+admin.site.register(Profile)
+admin.site.register(Customer)
+admin.site.register(Vehicle)
+admin.site.register(ServiceTicket)
+admin.site.register(ServiceCategory)
+admin.site.register(DefinedService)
+admin.site.register(ServiceOffered)
+admin.site.register(Part)
+admin.site.register(PartUsed)
+admin.site.register(Page)
--- auto_care/settings.py
+++ auto_care/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',  # Your app name here
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'app.CustomUser'
+
